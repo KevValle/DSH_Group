0b173f8f2d63cced587271daafdedccb436f47b3
KevValle <kevinvalle1@gmail.com>
2015-11-11 19:12:26 +0100

    Added Menu

---
+++ Animated Menu/Animated Menu/Assets/MenuNavigationScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MenuNavigationScript : MonoBehaviour {
+	
+	public RectTransform mainMenuPanel;    //reference for MainMenupanel
+	public Animator mainMenuAnimator;   //Animator reference
+	
+	// Update is called once per frame
+	public void Back () {
+		if((int)mainMenuPanel.localScale.x==0){ //check if scale of MainMenu Panel is 0
+			mainMenuAnimator.Play("MainMenuFadeIn"); //play fade in animation
+		}
+	}
+}
