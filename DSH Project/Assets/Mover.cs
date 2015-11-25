f3dc9fc308c300eea51056c086b96fb33caa0983
KevValle <kevinvalle1@gmail.com>
2015-11-25 16:48:37 +0100

    unity

---
+++ DSH Project/Assets/Mover.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Mover : MonoBehaviour {
+
+	public GameObject miCanvas;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void animacionMenu(){
+		miCanvas.GetComponent<Animator> ().enabled = true;
+	}
+}
